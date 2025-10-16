d1fbd419f962c48ef271bf8243b999f771f21f33
Josh Holman <josh@jolman.me>
2025-10-16 12:24:04 -0700

    finish initial script

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import sys
+import requests
 
-pokemon: str = sys.argv[1]
+# Optional error checking
+if len(sys.argv) != 2:
+    print("Error: expected one argument")
+    sys.exit(1)
 
-print(f"Hello, {pokemon}")
+# Read pokemon from command line args
+pokemon = sys.argv[1]
+
+print(f"Name: {pokemon}")
